db258026419e2efd02c73c20a5d0b9831e334a83
veritaserg <infophotomasterscomua@gmail.com>
2018-10-28 03:32:53 +0300

    save

---
+++ src/main/resources/populateDB.sql
@@ -0,0 +1,14 @@
+INSERT INTO developers
+VALUES (1, 'Ivanov Ivan', 2, 1800);
+
+INSERT INTO skills
+VALUES (1,'Java');
+
+INSERT INTO projects
+VALUES (1,'rozetka.ua');
+
+INSERT INTO companies
+VALUES (1,'sun');
+
+INSERT INTO customers
+VALUES (1,'rozetka');
