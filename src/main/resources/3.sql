2122d0fa9a93de9b4f600aeb360bb3a1e6212ad1
veritaserg <infophotomasterscomua@gmail.com>
2018-10-30 00:10:52 +0200

    save

--- src/main/resources/3.sql
+++ src/main/resources/3.sql
@@ -1,1 +1,5 @@
-SELECT SUM(SALLARY) FROM developers;
+SELECT SUM(SALLARY)
+FROM developers
+  INNER JOIN developers_skills ON developers.DEVELOPER_ID = developers_skills.DEVELOPER_ID
+INNER JOIN skills ON skills.SKILL_ID = developers_skills.SKILL_ID
+WHERE skills.NAME = (SELECT NAME FROM skills WHERE NAME LIKE 'Java');
