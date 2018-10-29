637bc14bbf1431b6cade408bfd4c2fb689a28605
veritaserg <infophotomasterscomua@gmail.com>
2018-10-29 20:35:56 +0200

    save

--- src/main/resources/populateDB.sql
+++ src/main/resources/populateDB.sql
@@ -1,14 +1,35 @@
 INSERT INTO developers
-VALUES (1, 'Ivanov Ivan', 2, 1800);
+VALUES (1, 'Ivanov ', 2, 1800);
+INSERT INTO developers
+VALUES (2, 'Petrov', 4, 200);
+INSERT INTO developers
+VALUES (3, 'Stepanov', 8, 2400);
+INSERT INTO developers
+VALUES (4, 'Jobs', 12, 4800);
+
 
 INSERT INTO skills
 VALUES (1,'Java');
+INSERT INTO skills
+VALUES (2,'C++');
+INSERT INTO skills
+VALUES (3,'JavaScript');
 
 INSERT INTO projects
-VALUES (1,'rozetka.ua');
+VALUES (1,'rozetka.ua',10000);
+INSERT INTO projects
+VALUES (2,'sinoptik.ua',500000);
+INSERT INTO projects
+VALUES (3,'mail.ru',70000);
 
 INSERT INTO companies
-VALUES (1,'sun');
+VALUES (1,'Luxoft');
+INSERT INTO companies
+VALUES (2,'GoIT');
 
 INSERT INTO customers
-VALUES (1,'rozetka');
+VALUES (1,'Stiv');
+INSERT INTO customers
+VALUES (2,'Bob');
+
+
